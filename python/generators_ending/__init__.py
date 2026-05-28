44c3d675d96467d266a9cf84b5ed9b97050c8264
wuhua2026 <abc2425000904@163.com>
2026-05-28 21:47:46 +0800

    feat: 初始化PPT模板资源库项目

---
+++ python/generators_ending/__init__.py
@@ -0,0 +1,11 @@
+"""结尾相关幻灯片生成器"""
+
+from python.generators_ending.thank_you import ThankYouGenerator
+from python.generators_ending.qr_code import QRCodeGenerator
+from python.generators_ending.contact import ContactPageGenerator
+
+__all__ = [
+    "ThankYouGenerator",
+    "QRCodeGenerator",
+    "ContactPageGenerator",
+]
