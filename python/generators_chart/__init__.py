a83eae77b65fa00842cab0fb67577f6bd3fff634
wuhua2026 <abc2425000904@163.com>
2026-05-28 21:46:48 +0800

    feat: 初始化PPT模板资源库项目

---
+++ python/generators_chart/__init__.py
@@ -0,0 +1,13 @@
+"""PPT图表生成器集合"""
+
+from python.generators_chart.bar_chart import BarChart
+from python.generators_chart.pie_chart import PieChart
+from python.generators_chart.line_chart import LineChart
+from python.generators_chart.animated_chart import AnimatedBarChart
+
+__all__ = [
+    "BarChart",
+    "PieChart",
+    "LineChart",
+    "AnimatedBarChart",
+]
