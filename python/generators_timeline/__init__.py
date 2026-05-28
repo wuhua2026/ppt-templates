e18f53ac292fb15d4638668023f7ac5cc9df0e8f
wuhua2026 <abc2425000904@163.com>
2026-05-28 21:48:01 +0800

    feat: 初始化PPT模板资源库项目

---
+++ python/generators_timeline/__init__.py
@@ -0,0 +1,13 @@
+"""PPT时间轴生成器集合"""
+
+from python.generators_timeline.dual_wave import DualWaveTimeline
+from python.generators_timeline.horizontal import HorizontalTimeline
+from python.generators_timeline.vertical import VerticalTimeline
+from python.generators_timeline.spiral import SpiralTimeline
+
+__all__ = [
+    "DualWaveTimeline",
+    "HorizontalTimeline",
+    "VerticalTimeline",
+    "SpiralTimeline",
+]
