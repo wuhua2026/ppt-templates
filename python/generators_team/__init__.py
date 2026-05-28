71c7c04bb3eccae0f4bb37a56e2351c7d4e94d81
wuhua2026 <abc2425000904@163.com>
2026-05-28 21:47:53 +0800

    feat: 初始化PPT模板资源库项目

---
+++ python/generators_team/__init__.py
@@ -0,0 +1,11 @@
+"""团队相关幻灯片生成器"""
+
+from python.generators_team.person_card import PersonCardGenerator
+from python.generators_team.team_grid import TeamGridGenerator
+from python.generators_team.org_chart import OrgChartGenerator
+
+__all__ = [
+    "PersonCardGenerator",
+    "TeamGridGenerator",
+    "OrgChartGenerator",
+]
