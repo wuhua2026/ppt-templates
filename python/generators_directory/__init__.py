e0404c752866a373358b7ef761621766562e9a7f
wuhua2026 <abc2425000904@163.com>
2026-05-28 21:47:40 +0800

    feat: 初始化PPT模板资源库项目

---
+++ python/generators_directory/__init__.py
@@ -0,0 +1,17 @@
+"""目录页生成器包"""
+
+from python.generators_directory.diamond_animated import DiamondAnimatedGenerator
+from python.generators_directory.circular_ring import CircularRingGenerator
+from python.generators_directory.hexagon import HexagonGenerator
+from python.generators_directory.sidebar import SidebarGenerator
+from python.generators_directory.card_grid import CardGridGenerator
+from python.generators_directory.timeline_style import TimelineStyleGenerator
+
+__all__ = [
+    "DiamondAnimatedGenerator",
+    "CircularRingGenerator",
+    "HexagonGenerator",
+    "SidebarGenerator",
+    "CardGridGenerator",
+    "TimelineStyleGenerator",
+]
